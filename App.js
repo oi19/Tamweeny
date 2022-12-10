d577dc7ae37eb0ad7304c77fc147906849cb1253
oi19 <omar2khaled.ok@gmail.com>
2022-12-10 13:32:03 +0200

    First commit

--- App.js
+++ App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import AppNav from './src/navigation/navigation';
 import { Provider } from 'react-redux'
-import { store } from './redux/configration';
+import { store } from './src/redux/configration';
 
 
 
