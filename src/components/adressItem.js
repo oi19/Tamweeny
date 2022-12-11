4e67762dd4012819d84605b5beba1b5775dcb95a
oi19 <omar2khaled.ok@gmail.com>
2022-12-11 14:45:34 +0200

    first commit

---
+++ src/components/adressItem.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { StyleSheet, View, Text } from "react-native";
+
+const adressItem = (props) => {
+    
+};
+
+const styles = StyleSheet.create({});
+
+export default adressItem;
